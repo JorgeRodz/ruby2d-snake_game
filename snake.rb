e9972d15ac8b060e5e9d532ad3899f92247b21d1
Jorge Rodriguez <jarf4321@gmail.com>
2022-11-09 11:31:37 -0600

    Add init gems and snake.rb file

---
+++ snake.rb
@@ -0,0 +1,14 @@
+# rubocop:disable all
+require 'ruby2d'
+
+# Set the window size
+set width: 300, height: 200
+
+# Create a new shape
+s = Square.new
+
+# Give it some color
+s.color = 'red'
+
+# Show the window
+show
