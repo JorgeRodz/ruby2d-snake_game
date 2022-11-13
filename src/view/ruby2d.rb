1cb5d3c37f996995ce3b6ab1b2fb5bc21b1873b4
Jorge Rodriguez <jarf4321@gmail.com>
2022-11-12 23:47:15 -0600

    Add-render snake initial state:
    - Add minitest in order to test it
    - Refactor app class in order to have a better structure

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -7,27 +7,34 @@ module View
     # Construct function/method
     def initialize
       @pixel_size = 50
+      @food
+      @snake_positions
     end
 
-    # Method to render all the game
-    def render(state)
+    # Method to display the window
+    def start(state)
       extend Ruby2D::DSL # Necessary to manage the native window system
       set( # Set window properties
         title: "Snake",
         width: @pixel_size * state.grid.cols,
         height: @pixel_size * state.grid.rows
       )
+      show # ruby2d method to display the window
+    end
+
+    # Method to render the game state(snake, food)
+    def render(state)
       render_food(state)  # To display the food
       render_snake(state)  # To display the snake
-      show # calling the method show in order to dislay the window
     end
 
     private
 
     def render_food(state)
+      @food.remove if @food
       extend Ruby2D::DSL # Necessary to manage the native window system
       food = state.food
-      Square.new(
+      @food = Square.new(
         x: food.col * @pixel_size,
         y: food.row * @pixel_size,
         size: @pixel_size,
@@ -36,9 +43,15 @@ module View
     end
 
     def render_snake(state)
+      # if @snake_positions
+      #   @snake_positions.each do |pos|
+      #     pos.remove
+      #   end
+      # end
+      @snake_positions.each(&:remove) if @snake_positions # This line is the same as the if block above
       extend Ruby2D::DSL # Necessary to manage the native window system
       snake = state.snake
-      snake.positions.each do |pos|
+      @snake_positions = snake.positions.map do |pos|
         Square.new(
           x: pos.col * @pixel_size,
           y: pos.row * @pixel_size,
