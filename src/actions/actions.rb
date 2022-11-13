89315ab74f736ee7a22371b3f286cd488538e4f1
Jorge Rodriguez <jarf4321@gmail.com>
2022-11-13 15:27:51 -0600

    Add-snake(grow) and movement, random generate food:
    - Implement the snake movement with arrow keys
    - Random generate the food when the snake eats it
    - Snake grows when eats the food
    - Game ends when collision with the wall or itself
    - Display the score when game ends

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,21 +1,49 @@
 # rubocop:disable all
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
+    next_direction = state.current_direction
     next_position = calc_next_position(state)
     # verificar que la siguiente casilla sea valida
-    if position_is_valid?(state, next_position)
+    if position_is_food?(state, next_position)
+      state = grow_snake_to(state, next_position)
+      generate_food(state)
+    elsif position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
     end
   end
 
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.current_direction = direction
+    else
+      puts "Invalid direction"
+    end
+    state
+  end
+
   private
+
+  def self.generate_food(state)
+    new_food = Model::Food.new(rand(state.grid.rows), rand(state.grid.cols))
+    state.food = new_food
+    state
+  end
+
+  def self.position_is_food?(state, next_position)
+    state.food.row == next_position.row && state.food.col == next_position.col
+  end
+
+  def self.grow_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions
+    state.snake.positions = new_positions
+    state
+  end
 
   def self.calc_next_position(state)
     current_position = state.snake.positions.first
-    case state.next_direction
+    case state.current_direction
       when Model::Direction::UP
         # reducir fila
         return Model::Coord.new(
@@ -64,5 +92,20 @@ module Actions
   def self.end_game(state)
     state.game_finished = true
     state
+  end
+
+  def self.next_direction_is_valid?(state, direction)
+    case state.current_direction
+      when Model::Direction::UP
+        return true if direction != Model::Direction::DOWN
+      when Model::Direction::DOWN
+        return true if direction != Model::Direction::UP
+      when Model::Direction::RIGHT
+        return true if direction != Model::Direction::LEFT
+      when Model::Direction::LEFT
+        return true if direction != Model::Direction::RIGHT
+    end
+
+    return false
   end
 end
